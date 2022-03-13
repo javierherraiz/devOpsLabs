915fba2c12ed82f1d7af4536ef2fe5334186c8f0
Javier Herraiz <elperrochico@gmail.com>
2022-03-13 10:58:34 +0100

    names fix

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "mySubnet" {
     count                  = length(var.vms)
     name                   = "terraformsubnet${var.vms[count.index]}"
     resource_group_name    = azurerm_resource_group.rg.name
-    virtual_network_name   = "azurerm_virtual_network.myNet.name
+    virtual_network_name   = azurerm_virtual_network.myNet.name
     address_prefixes       = "10.0.${count.index + 10}.0/24"
 
 }
