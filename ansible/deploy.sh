14a4f505cfc4888ef87e8e50ac758c630b85f2f3
Javier Herraiz <elperrochico@gmail.com>
2022-03-13 21:08:16 +0100

    first role and playbook

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-
-# añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l master-nfs 00-instalar-kubernetes-master.yaml
-ansible-playbook -i hosts -l worker playbook 01-instalar-kubernetes-worker.yaml
