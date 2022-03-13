0fd3f8eded21a64f9cc6ee4414a3eab41f69e47a
Javier Herraiz <elperrochico@gmail.com>
2022-03-13 10:21:21 +0100

    names fix

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "mySubnet" {
     count                  = length(var.vms)
     name                   = "terraformsubnet_${var.vms[count.index]}"
     resource_group_name    = azurerm_resource_group.rg.name
-    virtual_network_name   = azurerm_virtual_network.myNet_var.vms[count.index].name
+    virtual_network_name   = azurerm_virtual_network.myNet_${var.vms[count.index]}.name
     address_prefixes       = ["10.0.${count.index + 10}.0/24"]
 
 }
@@ -36,10 +36,10 @@ resource "azurerm_network_interface" "myNic" {
 
     ip_configuration {
     name                           = "myipconfiguration_${var.vms[count.index]}"
-    subnet_id                      = azurerm_subnet.mySubnet.id 
+    subnet_id                      = azurerm_subnet.mySubnet_${var.vms[count.index]}.id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "10.0.${count.index +10}.${count.index +10}"
-    public_ip_address_id           = azurerm_public_ip.myPublicIp.id
+    public_ip_address_id           = azurerm_public_ip.myPublicIp_${var.vms[count.index]}.id
   }
 
     tags = {
