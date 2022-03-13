5dd10bbd61f94dbe29b214ad29ab917e14142bf1
Javier Herraiz <elperrochico@gmail.com>
2022-03-13 11:10:45 +0100

    names fix

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "mySubnet" {
     count                  = length(var.vms)
     name                   = "terraformsubnet${var.vms[count.index]}"
     resource_group_name    = azurerm_resource_group.rg.name
-    virtual_network_name   = azurerm_virtual_network.myNet.name
+    virtual_network_name   = azurerm_virtual_network.myNet[count.index].name
     address_prefixes       = ["10.0.${count.index + 10}.0/24"]
 
 }
@@ -36,10 +36,10 @@ resource "azurerm_network_interface" "myNic" {
 
     ip_configuration {
     name                           = "myipconfiguration${var.vms[count.index]}"
-    subnet_id                      = azurerm_subnet.mySubnet.id 
+    subnet_id                      = azurerm_subnet.mySubnet[count.index].id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "10.0.${count.index +10}.${count.index +10}"
-    public_ip_address_id           = azurerm_public_ip.myPublicIp.id
+    public_ip_address_id           = azurerm_public_ip.myPublicIp[count.index].id
   }
 
     tags = {
@@ -53,7 +53,7 @@ resource "azurerm_network_interface" "myNic" {
 
 resource "azurerm_public_ip" "myPublicIp" {
   count               = length(var.vms) 
-  name                = "vmipvar.vms[count.index]"
+  name                = "vmipc${var.vms[count.index]}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
